7628b1923ec7b71f229ede48358c6116bd9a8c5e
FlutLab.io <>
2022-05-18 21:16:07 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/projeto1805/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projeto1805
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Dados'),
+          backgroundColor: Colors.blue,
+        ),
+        body: _novaPaginaDados(),
+      ),
+    ),
+  );
+}
+
+class _novaPaginaDados extends StatefulWidget {
+  const _novaPaginaDados({Key? key}) : super(key: key);
+
+  @override
+  State<_novaPaginaDados> createState() => new __novaPaginaDadosState();
+}
+
+class __novaPaginaDadosState extends State<_novaPaginaDados> {
+  Random random = new Random();
+  int ladoDado1 = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  ladoDado1 = random.nextInt(6) + 1;
+                });
+              },
+              child: Image.asset('images/dado$ladoDado1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
